a81b6d40a07cc6278d1e843d1c3f91aadec0d92a
Ifunanya (Funa) Nnoka <ifynnoka@gmail.com>
2019-01-06 14:13:04 -0800

    Update main.c

--- lesson1-Iterations/binaryGaps/binaryGaps/main.c
+++ lesson1-Iterations/binaryGaps/binaryGaps/main.c
@@ -10,62 +10,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *decimal_to_binary(int);
 int binary_gap(int);
 
 int main()
 {
     
     int n;
-    char a;
     char *pointer;
     int max_gap_len;
     
-    char *p;// = &a;
     while (1)
     {
         printf("Enter an Integer in Decimal Number System\n::::>>>>>>>");
         scanf("%d",&n);
-        pointer = decimal_to_binary(n);
-        printf("Binary String of Integer %d is: %s\n",n, pointer);
         max_gap_len = binary_gap(n);
         printf("Maximum Binary Gap Lenght of integer %d is: %d\n",n,max_gap_len);
         free(pointer);
-        p = &a;
-        printf("Continue? (y/n)\n::::>>>>>>>");
-        scanf("%s",p);
-        if (strcmp (p,"y") != 0)
-        break;
     }
     return 0;
-}
-
-
-char *decimal_to_binary(int n)
-{
-    int c, d, count;
-    char *pointer;
-    
-    count = 0;
-    pointer = (char*)malloc(32+1);
-    
-    if (pointer == NULL)
-        exit(EXIT_FAILURE);
-    
-    for (c = 31 ; c >= 0 ; c--)
-    {
-        d = n >> c;
-        
-        if (d & 1)
-            *(pointer+count) = 1 + '0';
-        else
-            *(pointer+count) = 0 + '0';
-        
-        count++;
-    }
-    *(pointer+count) = '\0';
-    
-    return  pointer;
 }
 
 
@@ -92,7 +54,6 @@ int binary_gap(int N)
                         max_width = zeros;
                     }
                 }
-               //ones = 0;
                 zeros = 0;
             }
             else
@@ -110,7 +71,7 @@ int binary_gap(int N)
             
         }
     }
-    printf("Number of gaps: %d\n", no_of_gaps);
+   // printf("Number of gaps: %d\n", no_of_gaps);
     return  max_width;
 }
 
